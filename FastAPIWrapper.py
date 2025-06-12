c308d2d5b7a449d0585f043a063683ddf6beebca
shsully99 <sean@sulltech.co.uk>
2025-06-12 18:57:06 +0100

    Added docstrings to endpoints

--- FastAPIWrapper.py
+++ FastAPIWrapper.py
@@ -31,21 +31,18 @@ class SetPeriodsRequest(BaseModel):
 # In-memory store for Survey objects, keyed by session or user id (for demo, use a global singleton)
 survey_store = {}
 
-@app.post("/survey/on_load")
-def survey_on_load():
-    # In a real-world app, verify user credentials here
-    session_id = str(uuid4())
-    #sions[session_id] = {"user": "example_user", "preferences": {}}
-    
-    # Set the session ID in a cookie
-    return "loaded"
-
 @app.post("/survey/logs")
 async def survey_logs(
     file: UploadFile = File(...),
     name: str = Form("name"),
     session_id: str = Form("session_id")
 ):
+    """
+    Add a Log object to the Survey object.
+    :param data: Initialised Log object
+    :param name: Name of the position, e.g. "A1"
+    :return: None.
+    """
     print(f"Adding log: {file.filename} with name: {name} for session_id: {session_id}")
     survey = survey_store.get(session_id)
     if survey is None:
@@ -67,6 +64,19 @@ async def survey_logs(
 
 @app.put("/survey/periods")
 def set_periods(req: SetPeriodsRequest):
+    """
+        Set the daytime, evening and night-time periods of all Log objects in the Survey.
+        To disable evening periods, simply set it the same as night-time.
+        :param times: A dictionary with strings as keys and integer tuples as values.
+        The first value in the tuple represents the hour of the day that period starts at (24hr clock), and the
+        second value represents the minutes past the hour.
+        e.g. for daytime from 07:00 to 19:00, evening 19:00 to 23:00 and night-time 23:00 to 07:00,
+        times = {"day": (7, 0), "evening": (19, 0), "night": (23, 0)}
+        NOTES:
+        Night-time must cross over midnight. (TBC experimentally).
+        Evening must be between daytime and night-time. To
+        :return: None.
+    """
     survey = survey_store.get(req.session_id)
     if survey is None:
         return {"error": f"No survey found for {req.session_id} . Please add a log first."}
@@ -78,6 +88,10 @@ def set_periods(req: SetPeriodsRequest):
 
 @app.get("/survey/periods")
 def get_periods(session_id: str):
+    """
+    Check the currently-set daytime, evening and night-time periods for each Log object in the Survey.
+    :return: Tuples of start times.
+    """    
     survey = survey_store.get(session_id)
     if survey is None:
         return {"error": "No survey found for this session. Please add a log first."}
@@ -91,7 +105,25 @@ def get_periods(session_id: str):
 
 @app.get("/survey/summary")
 def get_resi_summary(session_id: str):
-
+        """
+        Get a dataframe summarising the parameters relevant to assessment of internal ambient noise levels in
+        UK residential property assessments. Daytime and night-time Leqs, and nth-highest Lmax values all presented
+        in a succinct table. These will be summarised as per the daytime, evening and night-time periods set (default
+        daytime 07:00 to 23:00 and night-time 23:00 to 07:00).
+        The date of the Lmax values are presented for the night-time period beginning on that date. i.e. an Lmax
+        on 20/12/2024 would have occurred in the night-time period starting on that date and ending the following
+        morning.
+        :param leq_cols: List of tuples. The columns on which to perform Leq calculations. This can include L90
+        columns, or spectral values. e.g.  leq_cols = [("Leq", "A"), ("L90", "125")]
+        :param max_cols: List of tuples. The columns on which to get the nth-highest values.
+        Default max_cols = [("Lmax", "A")]
+        :param lmax_n: Int. The nth-highest value for max_cols. Default 10 for 10th-highest.
+        :param lmax_t: String. This is the time period over which to compute nth-highest Lmax values.
+        e.g. "2min" computes the nth-highest Lmaxes over 2-minute periods. Note that the chosen period must be
+        equal to or more than the measurement period. So you cannot measure in 5-minute periods and request 2-minute
+        Lmaxes.
+        :return: A dataframe presenting a summary of the Leq and Lmax values requested.
+        """
     print(f"Processing survey_resi_summary for session_id: {session_id}")
 
     survey = survey_store.get(session_id)
@@ -111,6 +143,26 @@ def get_resi_summary(session_id: str):
 
 @app.get("/survey/modal_l90")
 def survey_modal_l90(session_id: str):
+    """
+    Get a dataframe summarising Modal L90 values for each time period, as suggested by BS 4142:2014.
+    Currently, this method rounds the values to 0 decimal places by default and there is no alternative
+    implementation.
+    Note that this function will estimate L90s as a longer value by performing an Leq computation on them.
+    The measured data in Logs must be smaller than or equal to the desired period, i.e. you can't measure in 15-
+    minute periods and request 5-minute modal values.
+    :param cols: List of tuples of the columns desired. This does not have to be L90s, but can be any column.
+    :param by_date: Bool. If True, group the modal values by date. If False, present one modal value for each
+    period.
+    :param day_t:  String. Measurement period T. i.e. daytime measurements will compute modal values of
+    L90,60min by default.
+    :param evening_t: String. Measurement period T. i.e. evening measurements will compute modal values of
+    L90,60min by default, unless evenings are disabled (which they are by default).
+    :param night_t: Measurement period T. i.e. night-time measurements will compute modal values of
+    L90,15min by default.
+    :return: A dataframe of modal values for each time period.
+    """
+  
+
     survey = survey_store.get(session_id)
     if survey is None:
         return {"error": "No survey found for this session. Please add a log first."}
@@ -122,6 +174,24 @@ def survey_modal_l90(session_id: str):
 
 @app.get("/survey/lmax_spectra")
 def survey_lmax_spectra(session_id: str):
+
+    """
+    Get spectral data for the nth-highest Lmax values during a given time period.
+    This computes Lmax Event spectra. Lmax Hold spectra has not yet been implemented.
+    Assumptions and inputs as per Survey.resi_summary() method.
+    IMPORTANT: The dates of the Lmax values are presented for the night-time period beginning on that date.
+    This means that for early morning timings, the date is behind by one day.
+    e.g. an Lmax presented as occurring at 20/12/2024 at 01:22 would have occurred at 21/12/2024 at 01:22.
+    :param n: Int. Nth-highest Lmax. Default 10th-highest.
+    :param t: String. This is the time period over which to compute nth-highest Lmax values.
+    e.g. "2min" computes the nth-highest Lmaxes over 2-minute periods. Note that the chosen period must be
+    equal to or more than the measurement period. So you cannot measure in 5-minute periods and request 2-minute
+    Lmaxes.
+    :param period: String. "days", "evenings" or "nights"
+    :return: Dataframe of nth-highest Lmax Event spectra.
+    """
+
+
     survey = survey_store.get(session_id)
     if survey is None:
         return {"error": "No survey found for this session. Please add a log first."}
@@ -129,9 +199,15 @@ def survey_lmax_spectra(session_id: str):
     return result.to_dict()
 
 
-
 @app.get("/survey/typical_leq_spectra")
 def survey_typical_leq_spectra(session_id: str):
+    """
+    Compute Leqs over daytime, evening and night-time periods.
+    This is an overall Leq, and does not group Leqs by date.
+    :param leq_cols: List of strings or List of Tuples.
+    For all Leq columns, use ["Leq"]. For specific columns, use list of tuples [("Leq", "A"), ("Leq", 125)]
+    :return: A dataframe with a continuous Leq computation across dates, for each time period.
+    """
     survey = survey_store.get(session_id)
     if survey is None:
         return {"error": "No survey found for this session. Please add a log first."}
